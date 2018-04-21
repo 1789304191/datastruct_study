50b4a7bf8cbb3b3b45933abaf8a31a8cfc705bca
liguoxiong <1789304191@qq.com>
2018-04-21 15:58:30 +0800

    两个单链表合并的尾插法

--- datastruct_study/2_1.c
+++ datastruct_study/2_1.c
@@ -2,39 +2,48 @@
 #include <stdlib.h>
 #include<time.h>
 #define MAXSIZE 100
-typedef struct {
+typedef struct Student{
 	int num[MAXSIZE];
 	int length;
 } SqlList;
 
 
-int main() {
-	void print_array(int num[], int n);
-	void sort_array(SqlList * L);
-	int findElem(SqlList *L, int num);
-	void insertArray(SqlList *L, int num, int i);
-	SqlList sqlList;
-	SqlList *p=&sqlList;
-	p->length = 10;
-	//随机因子
-	srand((int)time(0));
-	for (int i = 0; i <= 10; i++) {
-		p->num[i] = rand() % 100;
-	}
-	printf("原数组：\n");
-	print_array(p->num, p->length);
-	sort_array(p);
-	printf("排序之后：\n");
-	print_array(p->num, p->length);
-	int a = rand() % 100;
-	printf("\n随机数为:%d\n", a);
-	printf("\n将要插入的位置:%d\n", findElem(p,a));
-	insertArray(p, a, findElem(p, a));
-	printf("\n插入之后:\n");
-	print_array(p->num, p->length);
-}
+//int main() {
+//	void print_array(int num[], int n);
+//	void sort_array(SqlList * L);
+//	int findElem(SqlList *L, int num);
+//	void insertArray(SqlList *L, int num, int i);
+//	void  deleteElment(SqlList *L, int p, int *e);
+//	SqlList sqlList;
+//	SqlList *p=&sqlList;
+//	p->length = 10;
+//	//随机因子
+//	srand((int)time(0));
+//	for (int i = 0; i <= 10; i++) {
+//		p->num[i] = rand() % 100;
+//	}
+//	printf("原数组：\n");
+//	print_array(p->num, p->length);
+//	sort_array(p);
+//	printf("排序之后：\n");
+//	print_array(p->num, p->length);
+//	int p1;
+//	int e;
+//	scanf_s("\n%d", &p1);
+//	deleteElment(p, p1, &e);
+//	printf("\n删除之后：%d\n",e);
+//	print_array(p->num, p->length);
+//	/*int a = rand() % 100;
+//	printf("\n随机数为:%d\n", a);
+//	printf("\n将要插入的位置:%d\n", findElem(p,a));
+//	insertArray(p, a, findElem(p, a));
+//	printf("\n插入之后:\n");
+//	print_array(p->num, p->length);*/
+//}
 
-
+/*
+	数组的插入
+*/
 void insertArray(SqlList *L,int num,int i) {
 	if (i == -1) {
 		L->num[L->length] = num;
@@ -48,6 +57,9 @@ void insertArray(SqlList *L,int num,int i) {
 	L->length = L->length + 1;
 }
 
+/*
+	数组的查找
+*/
 int findElem(SqlList *L,int num) {
 	int i=0;
 	for (i = 0; i < L->length; i++) {
@@ -82,4 +94,15 @@ void sort_array(SqlList *L) {
 			}
 		}
 	}
+}
+
+/*
+	数组的删除并把值赋值给e
+*/
+void  deleteElment(SqlList *L,int p,int *e) {
+	*e = L->num[p];
+	for (int i = p; i < L->length; i++) {
+		 L->num[i]=L->num[i + 1];
+	}
+	L->length = L->length - 1;
 }
---
+++ datastruct_study/Student.h
@@ -0,0 +1,20 @@
+﻿
+#ifndef _STUDENT_H_
+
+#define _STUDENT_H_
+
+
+
+//结构体
+
+struct Student {
+
+	int num;
+
+	float score;
+
+	struct Student * next;
+
+};
+
+#endif
