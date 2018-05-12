eac15a1563f778a3981b3132de2c14465afde029
liguoxiong <1789304191@qq.com>
2018-05-12 14:01:52 +0800

    稀疏矩阵的十字链表示方法

--- datastruct_study/Matrix.cpp
+++ datastruct_study/Matrix.cpp
@@ -34,3 +34,52 @@ void print(float b[][3]) {
 		}
 	}
 }
+
+
+void ArrayToCrossList(float a[][MAXSIZE], int m, int n, int k, CrossList &M) {
+	//1.赋值头节点的列行和非0数据
+	M.m = m;
+	M.n = n;
+	M.k = k;
+
+	//2.申请头节点数组空间所有要乘m和n,返回的是数组0的位置的地址
+	M.rhead = (OLNode *)malloc(sizeof(OLNode)*m);
+	M.chead = (OLNode *)malloc(sizeof(OLNode)*n);
+
+	//3.将头节点数组的right和down指针置空
+	for (int i = 0; i < m; i++) {
+		M.rhead[i].right = NULL;
+		M.rhead[i].down = NULL;
+	}
+
+	for (int j = 0; j < n; j++) {
+		M.chead[j].right = NULL;
+		M.chead[j].down = NULL;
+	}
+
+	//4.判断稀疏矩阵中的值是否为0，如果不为0则创建普通节点
+	OLNode * temp_r;
+	OLNode * temp_c;
+	for (int i = 0; i < m; i++) {
+		//临时指针
+		temp_r = &(M.rhead[i]);
+		for (int j = 0; j < n; j++) {
+			if (a[i][j] != 0) {
+				OLNode *p = NULL;
+				p = (OLNode *)malloc(sizeof(OLNode));
+				p->col = j;
+				p->row = i;
+				p->val = a[i][j];
+				p->down = NULL;
+				p->right = NULL;
+				//行指向p
+				temp_r->right = p;
+				temp_r = p;
+				//列指向p
+				temp_c = &(M.chead[j]);
+				temp_c->down = p;
+				temp_c = p;
+			}
+		}
+	}
+}
--- datastruct_study/Matrix.h
+++ datastruct_study/Matrix.h
@@ -4,10 +4,37 @@
 
 #define MAXSIZE 4
 
-//建立三元组 a为稀疏矩阵 b为三元组
+//十字链的普通节点
+typedef struct OLNode{ 
+
+	//所在行和列
+	int row,col;
+	//指向右方的节点的指针
+	OLNode * right;
+	//指向下方节点的指针
+	OLNode * down;
+	//稀疏矩阵中的值
+	float val;
+
+} OLNode ;
+
+typedef struct CrossList {
+
+	//m为稀疏矩阵的行数，n为稀疏矩阵的列数，k为稀疏矩阵中有多少非0元素
+	int m, n,k;
+
+	//指向两个头节点数组的指针
+	OLNode * rhead, * chead;
+
+} CrossList ;
+
+//建立三元组 a为稀疏矩阵 b为三元组 m为矩阵的行数，n为矩阵的列数
 void createTrimat(float a[][MAXSIZE], int m, int n, float b[][3]);
 
 //3元组的打印
 void print(float b[][3]);
+
+//稀疏矩阵的十字链表存储结构，a为稀疏矩阵，m为行数，n为列数，k为非0节点的个数 M为头节点
+void ArrayToCrossList(float a[][MAXSIZE],int m,int n,int k, CrossList &M);
 
 #endif
